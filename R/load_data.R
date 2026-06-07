8a5390a2db1a699a7fe8f08a1b33027e4eddd652
Amanda Dong <amandaadong15@gmail.com>
2026-06-06 23:26:46 -0700

    finish project 3

--- R/load_data.R
+++ R/load_data.R
@@ -1,9 +1,11 @@
 #' Load data set of user's choice (helper function)
 #'
+#' @param string `cases_month`, `cases_year`, or `airquality` file to be loaded
+#'
 #' @return A tibble containing measles data
 #' @importFrom stringr str_c
 #' @export
-load_data <- function(file) {
+load_data <- function(file = "cases_month") {
   if (!file %in% c("cases_month", "cases_year", "airquality")) {
     stop("Data file not found. Enter either 'cases_month', 'cases_year', or 'airquality'.")
   }
